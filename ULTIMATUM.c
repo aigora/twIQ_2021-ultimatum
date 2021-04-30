381a3b6bc64a9418d008b51d59c1194e44915d5a
susanaramos02 <80400988+susanaramos02@users.noreply.github.com>
2021-04-30 14:27:57 +0200

    Update ULTIMATUM.c

--- ULTIMATUM.c
+++ ULTIMATUM.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
 #include <conio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-struct figuras { 
-	char nombre[30]; 
-	int num;  
-}; 
-
+struct figuras{
+	char nombre[30];
+	int num;
+};
 
 int main(){
-	
-	int opcion;
-	int i,j=0;
-	int J1,J2,num,numero;
-	//Enumeramos las figuras en un orden que hemos preestablecido
+    char opcion;
+    int J1, J2,num,numero,i,j=0;
+
 	struct figuras coleccion[15]={ {"Piedra",1}, {"Fuego",2}, {"Tijeras",3}, {"Serpiente",4}, {"Humano",5}, {"Arbol",6}, {"Lobo",7}, {"Esponja",8}, {"Papel",9}, {"Aire",10}, {"Agua",11}, {"Dragon",12}, {"Demonio",13}, {"Rayo",14}, {"Pistola",15} };
-				
+	
 	char frase1[] = "En este juego a veces lo absurdo es superior a la realidad, no busques sentido a la logica porque ni nosotros\n lo sabemos, pero lo hemos intentado dar una explicacion.\n A continuacion te diremos que figura gana a quien y tratarle de dar una cierta logica.\n";
 	char frase2[] = "\nAire: 'Has invocado tifon, Dios del viento y de los huracanes, enemigo de Zeus y desterrado al tartaro por este mismo.'\n";
 	char frase3[] = "\nDiablo: 'Has invocado al principe de las tinieblas, rey del mal.Â¿Preparados para vender tu alma?'\n";
@@ -32,61 +29,65 @@ int main(){
 	char frase14[] = "\nHumano: 'Has invocado al todopoderoso Napoleon, Emperador de Europa.'\n";
 	char frase15[] = "\nAgua: 'Has invocado a las poderosas corrientes del triangulo de las Bermudas, responsable de gran cantidad de misteriosas desapariciones.'\n";
 	char frase16[] = "\nRayo: 'Has invocado al arma más poderosa y destructiva de Zeus el Trueno'\n";
+	
 	printf( "\n   //// ULTIMATUM ////\n" );
 	printf("Bienvenido jugador valiente.\n En este nueva version de piedra, papel o tijera con mas variantes podras experimentar un \n nuevo mundo de diversion.\n");
 	printf("Decide que quieres hacer a continuacion:\n");
 
     do
     {
-        printf( "\n   >>> MENU DEL JUEGO  <<<" );
-        printf( "\n\n   1. J1 vs ORDERANDOR.");
-        printf( "\n   2. RANKING.");
-        printf( "\n   3. REGLAS.");
-        printf( "\n   4. SALIR.\n" );
+        printf( "\n   >>> MENU DEL JUEGO <<<");
+        printf( "\n\n   1. J1 vs ORDENADOR");
+        printf( "\n   2. RANKING");
+        printf( "\n   3. REGLAS");
+        printf( "\n   4. SALIR" );
 
+        /* Filtramos la opción elegida por el usuario */
         do
         {
             printf( "\n   Introduzca opcion (1-4): ");
-            fflush ( stdin );
+            fflush( stdin );
             scanf( "%c", &opcion);
-            
-        }while ( opcion < '1' || opcion > '4' );
+
+        } while ( opcion < '1' || opcion > '4' );
+        /* La opción sólo puede ser '1', '2', '3' o '4'*/
 
         switch ( opcion )
         {
-                      
-            case '1': 
-		printf("1.Piedra 2.Fuego  3.Tijera  4.Serpiente  5.Humano  6.Arbol  7.Lobo  8.Esponja  9.Papel  10.Aire  11.Agua  12.Dragon  13.Demonio  14.Rayo  15.Pistola\n");
-		while(j<20) { //20 es el número de rondas que tiene cada partida.
-			printf("Seleccione su %d figura J1: ", j+1); 
-			scanf("%d",&num);  
-				for(i=0;i<15;i++) { 
-					if(num==coleccion[i].num) { 
-						printf("Su eleccion ha sido:"); 
-						printf("%s\n",coleccion[i].nombre); 
-					} 
-					J1=num;
-				} 			
-				srand(time(NULL)); //El jugador 1 ha elegido su personaje
-        			int numero = rand() % 16; //Elección aleatoria de la computadora
-        			printf("La maquina ha seleccionado %d\n" ,numero);
-		            	for(i=0;i<15;i++) { 
-		                	if(numero==coleccion[i].num) { 
-        		            		printf("EL todo poderoso ha seleccionado:"); 
-                		    		printf("%s\n",coleccion[i].nombre); 
-                			}
-            			} 
-				J2=numero;
-            			printf("%d %d\n", J1,J2);
-        			j++;
-   			}		
-           		 break;   
-            case '2': 
-		printf("\n ***RANKING*** \n");
-            break;    
-            case '3': 	
-		printf( "%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n", frase1,frase2,frase3,frase4,frase5,frase6,frase7,frase8,frase9,frase10,frase11,frase12,frase13,frase14,frase15,frase16); 
-            break;
+                      /* Opción 1: J1 vs ORDENADOR */
+            case '1': printf("1.Piedra 2.Fuego  3.Tijera  4.Serpiente  5.Humano  6.Arbol  7.Lobo  8.Esponja  9.Papel  10.Aire  11.Agua  12.Dragon  13.Demonio  14.Rayo  15.Pistola\n");
+                      while(j<20) {
+                      	printf("Seleccione su %d figura J1: ", j+1); 
+						scanf("%d",&num);  
+						for(i=0;i<15;i++){
+							if(num==coleccion[i].num){
+								printf("Su eleccion ha sido:"); 
+								printf("%s\n",coleccion[i].nombre); 
+							}
+							J1=num;
+						}
+						srand(time(NULL));
+						int numero = rand() % 16;
+						printf("La maquina ha seleccionado %d\n" ,numero);
+						for(i=0;i<15;i++){
+							if(numero==coleccion[i].num){
+								printf("EL todo poderoso ha seleccionado:"); 
+                		    	printf("%s\n",coleccion[i].nombre); 
+							}
+							J2=numero;
+						}
+						printf("%d %d\n", J1,J2);
+						j++;
+					  	}
+                      break;
+
+                      /* Opción 2: RANKING */
+            case '2': printf("\n ***RANKING*** \n");
+                      break;
+
+                      /* Opción 3: REGLAS */
+            case '3': printf( "%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n", frase1,frase2,frase3,frase4,frase5,frase6,frase7,frase8,frase9,frase10,frase11,frase12,frase13,frase14,frase15,frase16); 
+                      break;
         }
 
     } while ( opcion != '4' );
